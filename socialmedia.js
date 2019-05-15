f39898ea48b9a1440a9882fc118bd3223d172969
Aldane Purrier <aldanepur@gmail.com>
2019-05-15 14:30:20 -0400

    social media headline

---
+++ socialmedia.js
@@ -0,0 +1,24 @@
+class User {
+  constructor(email, password) {
+    this.email = email
+    this.password = password
+    this.inbox = []
+  }
+
+  sendMessage() {
+    let message = new Message ()
+    rec.inbox.push(msg)
+  }
+}
+
+class Message {
+  constructor(content) {
+    this.content = prompt('Enter a message')
+  }
+}
+
+user1 = new User('aldane@example.com','1111111')
+
+user2 = new User('greninja@smash.com','1111111')
+
+msg = new Message('Why do the Knicks suck?')
